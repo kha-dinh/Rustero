548e394963d8c79e752e602a50e202c9f68f3656
khadd <dalo2903@gmail.com>
2023-02-15 01:25:15 +0900

    Add code for prototype UI, loading creators from db

---
+++ src/db_connector.rs
@@ -0,0 +1,103 @@
+use sqlx::{query_as, SqlitePool};
+// use sqlx::sql
+
+#[derive(Debug, Clone)]
+pub struct Document {
+    pub item_data: ItemData,
+    pub creators: Vec<Creator>,
+}
+impl FromIterator<ItemData> for Vec<Document> {
+    fn from_iter<T: IntoIterator<Item = ItemData>>(iter: T) -> Self {
+        iter.into_iter()
+            .map(|item| Document {
+                item_data: item.clone(),
+                creators: Vec::new(),
+            })
+            .collect()
+    }
+}
+#[derive(Debug, Clone)]
+#[allow(non_snake_case)]
+pub struct ItemData {
+    itemId: i64,
+    pub title: String,
+    pub abstracttext: String,
+    pub pubdate: String,
+    pub key: String,
+}
+
+#[derive(Debug, Clone)]
+#[allow(non_snake_case)]
+pub struct Creator {
+    pub firstName: String,
+    pub lastName: String,
+}
+
+#[allow(non_snake_case)]
+pub async fn get_creators(pool: &SqlitePool, docs: &mut Vec<Document>) -> anyhow::Result<()> {
+    for doc in docs {
+        let records = query_as!(
+            Creator,
+            r#"
+SELECT firstName as "firstName!", lastName as  "lastName!" 
+FROM creators JOIN itemCreators on itemCreators.creatorID = creators.creatorID
+WHERE itemID = ?
+"#,
+            doc.item_data.itemId
+        )
+        .fetch_all(pool)
+        .await?;
+        doc.creators.clone_from(&records);
+    }
+    Ok(())
+}
+
+#[allow(non_snake_case)]
+pub async fn get_all_item_data(
+    pool: &SqlitePool,
+    item_data: &mut Vec<ItemData>,
+) -> anyhow::Result<()> {
+    // let conn = pool.acquire().await?;
+    // let docs = Vec::new();
+    let records = query_as!(
+    ItemData,
+        r#"
+SELECT d1.itemID as "itemId!", 
+    title as "title!", 
+    key as "key!", 
+    abstract as "abstracttext!", 
+    pubdate as "pubdate!" 
+FROM 
+	(SELECT value as title,itemID	from itemDataValues JOIN itemData on itemDataValues.valueID = itemData.valueID WHERE fieldID = 1) as d1
+	JOIN (SELECT value as abstract, itemID	from itemDataValues JOIN itemData on itemDataValues.valueID = itemData.valueID WHERE fieldID = 2) as d2 ON d1.itemID = d2.itemID
+	JOIN (SELECT value as pubdate, itemID	from itemDataValues JOIN itemData on itemDataValues.valueID = itemData.valueID WHERE fieldID = 6) as d3 ON d1.itemID = d3.itemID	
+    JOIN items ON items.itemID = d1.itemID  
+"#
+    )
+    .fetch_all(pool)
+    .await?;
+    item_data.clone_from(&records);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+
+    use super::*;
+    #[test]
+    fn test() {
+        dotenv::dotenv().ok();
+        let mut all_items = Vec::new();
+
+        let url = env::var("DATABASE_URL");
+        let pool = tokio_test::block_on(SqlitePool::connect(&url.unwrap())).unwrap();
+        tokio_test::block_on(get_all_item_data(&pool, &mut all_items))
+            .expect("Expect read all docs");
+        // dbg!(&all_items);
+        let mut all_docs: Vec<Document> = Vec::from_iter(all_items);
+
+        tokio_test::block_on(get_creators(&pool, &mut all_docs)).expect("Expect read all docs");
+        dbg!(all_docs);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod db_connector;
+mod event;
+mod user_config;
+use anyhow::Result;
+
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use db_connector::Document;
+use event::Key;
+use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
+use sqlx::SqlitePool;
 /// A simple example demonstrating how to handle user input. This is
 /// a bit out of the scope of the library as it does not provide any
 /// input handling out of the box. However, it may helps some to get
@@ -9,61 +23,143 @@
 ///   * Pressing Backspace erases a character
 ///   * Pressing Enter pushes the current input in the history of previous
 ///   messages
-use event::*;
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
-use std::{error::Error, io};
+use std::{env, error::Error, io};
+use tokio;
 use tui::{
     backend::{Backend, CrosstermBackend},
     layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
     text::{Span, Spans, Text},
-    widgets::{Block, Borders, List, ListItem, Paragraph},
+    widgets::{Block, Borders, List, ListItem, ListState, Paragraph},
     Frame, Terminal,
 };
-mod db_connector;
 use unicode_width::UnicodeWidthStr;
+use user_config::UserConfig;
 
-enum InputMode {
-    Normal,
-    Editing,
+struct StatefulList<T> {
+    state: ListState,
+    items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn unselect(&mut self) {
+        self.state.select(None);
+    }
 }
 
 /// App holds the state of the application
 struct App {
     /// Current value of the input box
-    input: String,
-    /// Current input mode
-    input_mode: InputMode,
+    search_input: String,
+    sqlite_pool: Option<SqlitePool>,
     /// History of recorded messages
-    messages: Vec<String>,
+    documents: StatefulList<Document>,
+    // documents_state: StatefulList<Document>,
 }
 
 impl Default for App {
     fn default() -> App {
         App {
-            input: String::new(),
-            input_mode: InputMode::Normal,
-            messages: Vec::new(),
+            search_input: String::new(),
+            sqlite_pool: None,
+            documents: StatefulList {
+                state: ListState::default(),
+                items: Vec::new(),
+            },
         }
     }
 }
+impl App {
+    fn update_on_tick(&self) {}
+}
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // setup terminal
+async fn start_ui(user_config: UserConfig) -> Result<()> {
+    dotenv::dotenv().ok();
+    let url = env::var("DATABASE_URL");
+
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
     let backend = CrosstermBackend::new(stdout);
+
     let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
 
     // create app and run it
-    let app = App::default();
-    let res = run_app(&mut terminal, app);
+    let events = event::Events::new(user_config.behavior.tick_rate_milliseconds);
+    let mut app = App::default();
+    app.sqlite_pool = Option::from(SqlitePool::connect(&url.unwrap()).await?);
+    let mut item_data = Vec::new();
+    db_connector::get_all_item_data(app.sqlite_pool.as_mut().unwrap(), &mut item_data).await?;
+    app.documents.items = Vec::from_iter(item_data);
 
+    let mut is_first_render = true;
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+        if is_first_render {
+            is_first_render = false;
+        }
+        match events.next()? {
+            event::Event::Input(key) => {
+                if key == Key::Ctrl('c') {
+                    break;
+                }
+                match key {
+                    Key::Down => app.documents.next(),
+                    Key::Up => app.documents.previous(),
+                    Key::Backspace => {
+                        app.search_input.pop();
+                        app.documents.state.select(Some(0));
+                    }
+                    Key::Char(c) => {
+                        app.search_input.push(c);
+                        app.documents.state.select(Some(0));
+                    }
+                    _ => {}
+                }
+            }
+
+            event::Event::Tick => {
+                app.update_on_tick();
+            }
+        }
+    }
     // restore terminal
     disable_raw_mode()?;
     execute!(
@@ -72,123 +168,185 @@ fn main() -> Result<(), Box<dyn Error>> {
         DisableMouseCapture
     )?;
     terminal.show_cursor()?;
-
-    if let Err(err) = res {
-        println!("{:?}", err)
-    }
-
     Ok(())
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
-    loop {
-        terminal.draw(|f| ui(f, &app))?;
+#[tokio::main]
+async fn main() -> Result<()> {
+    // setup terminal
+    let mut user_config = UserConfig::new();
 
-        if let Event::Key(key) = event::read()? {
-            match app.input_mode {
-                InputMode::Normal => match key.code {
-                    KeyCode::Char('e') => {
-                        app.input_mode = InputMode::Editing;
-                    }
-                    KeyCode::Char('q') => {
-                        return Ok(());
-                    }
-                    _ => {}
-                },
-                InputMode::Editing => match key.code {
-                    KeyCode::Enter => {
-                        app.messages.push(app.input.drain(..).collect());
-                    }
-                    KeyCode::Char(c) => {
-                        app.input.push(c);
-                    }
-                    KeyCode::Backspace => {
-                        app.input.pop();
-                    }
-                    KeyCode::Esc => {
-                        app.input_mode = InputMode::Normal;
-                    }
-                    _ => {}
-                },
-            }
-        }
-    }
+    start_ui(user_config).await?;
+    Ok(())
 }
 
-fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
-    let chunks = Layout::default()
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    let main_layout = Layout::default()
         .direction(Direction::Vertical)
-        .margin(2)
+        .margin(1)
+        .constraints([Constraint::Length(3), Constraint::Min(1)].as_ref())
+        .split(f.size());
+
+    f.set_cursor(
+        // Put cursor past the end of the input text
+        main_layout[0].x + app.search_input.width() as u16 + 1,
+        // Move one line down, from the border to the input line
+        main_layout[0].y + 1,
+    );
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
         .constraints(
             [
-                Constraint::Length(1),
-                Constraint::Length(3),
-                Constraint::Min(1),
+                Constraint::Percentage(20),
+                Constraint::Percentage(50),
+                Constraint::Percentage(20),
+                Constraint::Percentage(10),
             ]
             .as_ref(),
         )
-        .split(f.size());
+        .split(main_layout[1]);
 
-    let (msg, style) = match app.input_mode {
-        InputMode::Normal => (
-            vec![
-                Span::raw("Press "),
-                Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to exit, "),
-                Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to start editing."),
-            ],
-            Style::default().add_modifier(Modifier::RAPID_BLINK),
-        ),
-        InputMode::Editing => (
-            vec![
-                Span::raw("Press "),
-                Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to stop editing, "),
-                Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
-                Span::raw(" to record the message"),
-            ],
-            Style::default(),
-        ),
-    };
-    let mut text = Text::from(Spans::from(msg));
-    text.patch_style(style);
-    let help_message = Paragraph::new(text);
-    f.render_widget(help_message, chunks[0]);
+    // let test = Layout::default()
+    //     .margin(2)
+    //     .constraints([Constraint::Percentage(30), Constraint::Percentage(70)].as_ref())
+    //     .split(chunks[1]);
+    //
 
-    let input = Paragraph::new(app.input.as_ref())
-        .style(match app.input_mode {
-            InputMode::Normal => Style::default(),
-            InputMode::Editing => Style::default().fg(Color::Yellow),
-        })
+    let input = Paragraph::new(app.search_input.as_ref())
         .block(Block::default().borders(Borders::ALL).title("Input"));
-    f.render_widget(input, chunks[1]);
-    match app.input_mode {
-        InputMode::Normal =>
-            // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
-            {}
+    f.render_widget(input, main_layout[0]);
 
-        InputMode::Editing => {
-            // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
-            f.set_cursor(
-                // Put cursor past the end of the input text
-                chunks[1].x + app.input.width() as u16 + 1,
-                // Move one line down, from the border to the input line
-                chunks[1].y + 1,
-            )
-        }
-    }
+    let matcher = SkimMatcherV2::default();
 
-    let messages: Vec<ListItem> = app
-        .messages
-        .iter()
-        .enumerate()
-        .map(|(i, m)| {
-            let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
-            ListItem::new(content)
+    let mut filtered_doc = app.documents.items.iter().filter(|doc| {
+        // Match fuzzy find
+        matcher
+            .fuzzy_match(&doc.item_data.title, app.search_input.as_str())
+            .is_some()
+    });
+
+    let titles: Vec<ListItem> = filtered_doc
+        .clone()
+        .map(|doc| {
+            let header = Span::raw(&doc.item_data.title);
+            ListItem::new(header)
         })
         .collect();
-    let messages =
-        List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
-    f.render_widget(messages, chunks[2]);
+
+    let titles = List::new(titles)
+        .block(Block::default().borders(Borders::ALL).title("List"))
+        .highlight_style(
+            Style::default()
+                .bg(Color::LightGreen)
+                .fg(Color::Black)
+                .add_modifier(Modifier::BOLD),
+        );
+
+    let pubdate: Vec<ListItem> = filtered_doc
+        .clone()
+        .map(|doc| {
+            let header = Span::raw(&doc.item_data.pubdate[..4]);
+            ListItem::new(header)
+        })
+        .collect();
+
+    let pubdate = List::new(pubdate)
+        .block(Block::default().borders(Borders::ALL).title("List"))
+        .highlight_style(
+            Style::default()
+                .bg(Color::LightGreen)
+                .fg(Color::Black)
+                .add_modifier(Modifier::BOLD),
+        );
+    let author: Vec<ListItem> = filtered_doc
+        .clone()
+        .map(|doc| {
+            let header = Span::raw(&doc.creators[0].firstName);
+            ListItem::new(header)
+        })
+        .collect();
+
+    let author = List::new(author)
+        .block(Block::default().borders(Borders::ALL).title("creator"))
+        .highlight_style(
+            Style::default()
+                .bg(Color::LightGreen)
+                .fg(Color::Black)
+                .add_modifier(Modifier::BOLD),
+        );
+
+    f.render_stateful_widget(titles, chunks[1], &mut app.documents.state);
+    f.render_stateful_widget(pubdate, chunks[3], &mut app.documents.state);
+    f.render_stateful_widget(author, chunks[2], &mut app.documents.state);
+
+    // let documents = List::new(app.documents);
+    // let chunks = Layout::default()
+    //     .direction(Direction::Vertical)
+    //     .margin(2)
+    //     .constraints(
+    //         [
+    //             Constraint::Length(1),
+    //             Constraint::Length(3),
+    //             Constraint::Min(1),
+    //         ]
+    //         .as_ref(),
+    //     )
+    //     .split(f.size());
+    // .split(f.size());
+
+    // let (msg, style) = match app.input_mode {
+    //     InputMode::Normal => (
+    //         vec![
+    //             Span::raw("Press "),
+    //             Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
+    //             Span::raw(" to exit, "),
+    //             Span::styled("e", Style::default().add_modifier(Modifier::BOLD)),
+    //             Span::raw(" to start editing."),
+    //         ],
+    //         Style::default().add_modifier(Modifier::RAPID_BLINK),
+    //     ),
+    //     InputMode::Editing => (
+    //         vec![
+    //             Span::raw("Press "),
+    //             Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
+    //             Span::raw(" to stop editing, "),
+    //             Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
+    //             Span::raw(" to record the message"),
+    //         ],
+    //         Style::default(),
+    //     ),
+    // };
+    // let mut text = Text::from(Spans::from(msg));
+    // text.patch_style(style);
+    // let help_message = Paragraph::new(text);
+    // f.render_widget(help_message, chunks[0]);
+
+    // match app.input_mode {
+    //     InputMode::Normal =>
+    //         // Hide the cursor. `Frame` does this by default, so we don't need to do anything here
+    //         {}
+
+    //     InputMode::Editing => {
+    //         // Make the cursor visible and ask tui-rs to put it at the specified coordinates after rendering
+    //         f.set_cursor(
+    //             // Put cursor past the end of the input text
+    //             chunks[1].x + app.input.width() as u16 + 1,
+    //             // Move one line down, from the border to the input line
+    //             chunks[1].y + 1,
+    //         )
+    //     }
+    // }
+
+    // let messages: Vec<ListItem> = app
+    //     .messages
+    //     .iter()
+    //     .enumerate()
+    //     .map(|(i, m)| {
+    //         let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
+    //         ListItem::new(content)
+    //     })
+    //     .collect();
+    // let messages =
+    //     List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
+    // f.render_widget(messages, chunks[2]);
 }
